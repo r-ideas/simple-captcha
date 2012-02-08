982d6f0f4cfd20dd8aa3f15be71ca2564262eec6
Nadya Shevtsova <nshevtsova@r-ideas.org>
2012-02-08 18:55:23 +0200

    fix follow rails 3

--- lib/simple_captcha/active_record.rb
+++ lib/simple_captcha/active_record.rb
@@ -51,7 +51,7 @@ module SimpleCaptcha #:nodoc
 
       def valid_with_captcha?
         checks = [valid?]
-        checks << is_captcha_valid? if RAILS_ENV == 'test'
+        checks << is_captcha_valid? if ::Rails.env.test?
         checks.all?
       end
 
